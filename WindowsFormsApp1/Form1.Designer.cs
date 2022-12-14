1d52e7c3f6ab596176652c9ff3a2d36afe95437f
omar2512 <113863685+omar2512@users.noreply.github.com>
2022-12-14 20:26:42 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnminus = new System.Windows.Forms.Button();
+            this.btnmulti = new System.Windows.Forms.Button();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(11, 27);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(352, 22);
+            this.num1.TabIndex = 0;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(44, 288);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(124, 89);
+            this.btnadd.TabIndex = 1;
+            this.btnadd.Text = "+";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnminus
+            // 
+            this.btnminus.Location = new System.Drawing.Point(239, 288);
+            this.btnminus.Name = "btnminus";
+            this.btnminus.Size = new System.Drawing.Size(124, 89);
+            this.btnminus.TabIndex = 2;
+            this.btnminus.Text = "-";
+            this.btnminus.UseVisualStyleBackColor = true;
+            this.btnminus.Click += new System.EventHandler(this.btnminus_Click);
+            // 
+            // btnmulti
+            // 
+            this.btnmulti.Location = new System.Drawing.Point(437, 288);
+            this.btnmulti.Name = "btnmulti";
+            this.btnmulti.Size = new System.Drawing.Size(124, 89);
+            this.btnmulti.TabIndex = 3;
+            this.btnmulti.Text = "*";
+            this.btnmulti.UseVisualStyleBackColor = true;
+            this.btnmulti.Click += new System.EventHandler(this.btnmulti_Click);
+            // 
+            // btndiv
+            // 
+            this.btndiv.Location = new System.Drawing.Point(625, 288);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(124, 89);
+            this.btndiv.TabIndex = 4;
+            this.btndiv.Text = "/";
+            this.btndiv.UseVisualStyleBackColor = true;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(437, 27);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(352, 22);
+            this.num2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 6;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(369, 93);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 17);
+            this.result.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnmulti);
+            this.Controls.Add(this.btnminus);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnminus;
+        private System.Windows.Forms.Button btnmulti;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        float resultss;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text = btnadd.Text;
+            if(num1 != null && num2 != null)
+            {
+                resultss = float.Parse((num1.Text)) + float.Parse((num2.Text));
+
+                result.Text = resultss.ToString();
+            }
+            else
+            {
+                MessageBox.Show("check if the two numbers are entered");
+            }
+        }
+
+        private void btnminus_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text = btnminus.Text;
+            if (num1 != null && num2 != null)
+            {
+                resultss = float.Parse((num1.Text)) - float.Parse((num2.Text));
+
+                result.Text = resultss.ToString();
+            }
+            else
+            {
+                MessageBox.Show("check if the two numbers are entered");
+            }
+        }
+
+        private void btnmulti_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text = btnmulti.Text;
+            if (num1 != null && num2 != null)
+            {
+                resultss = float.Parse((num1.Text)) * float.Parse((num2.Text));
+
+                result.Text = resultss.ToString();
+            }
+            else
+            {
+                MessageBox.Show("check if the two numbers are entered");
+            }
+        }
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label1.Text = btndiv.Text;
+
+            if (num1 != null && num2 != null)
+            {
+                if (int.Parse(num2.Text) == 0)
+                {
+                    MessageBox.Show("cant divsion by zero");
+                }
+                else
+                {
+                    resultss = float.Parse((num1.Text)) / float.Parse((num2.Text));
+
+                    result.Text = resultss.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("check if the two numbers are entered");
+            }   }
+    }
+}
